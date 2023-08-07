36f4d0d8634db36e0eed01bfc8819fb21d6899ed
ChaurasiaP <pranshu1431@gmail.com>
2023-08-07 11:02:49 +0530

    added IMG folder, dependency in pubspec.yaml for Quiz App

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,288 +1,146 @@
 import 'package:biz_card/Util/HexColor.dart';
 import 'package:flutter/material.dart';
+import '/model/Question.dart';
 
-class BillSplitter extends StatefulWidget {
-  const BillSplitter({super.key});
-
+class QuizApp extends StatefulWidget {
   @override
-  _BillSplitterState createState() => _BillSplitterState();
+  _QuizAppState createState() => _QuizAppState();
 }
 
-class _BillSplitterState extends State<BillSplitter> {
+class _QuizAppState extends State<QuizApp> {
+  int _currentQuestionIndex = 0;
 
-  int _tipPercentage = 0;
-  int _personCounter = 1;
-  double _billAmount = 0.0;
+  List questionBank = [
+    Question.name("Is Paris the capital city of France?", true),
+    Question.name(
+        "Did William Shakespeare write the play \"Romeo and Juliet\"?", true),
+    Question.name("Is Saturn known as the \"Red Planet\"?", false),
+    Question.name("Is the chemical symbol for water H2O?", true),
+    Question.name("Is the Blue Whale the largest mammal on Earth?", true),
+    Question.name("Did Picaso paint the Mona Lisa?", false),
+    Question.name("Is the currency of India the Rupee?", true),
+    Question.name(
+        "Did Albert Einstein develop the theory of relativity?", true),
+    Question.name(
+        "Is Mount Kiliminjaro the tallest mountain in the world?", false),
+    Question.name("Are there seven continents on Earth?", true),
+  ];
 
-  Color _purple = HexColor("#6908D6");
+  List imgList = [
+    Image.asset("img/1.jpg"),
+    Image.asset("img/2.jpg"),
+    Image.asset("img/3.jpg"),
+    Image.asset("img/4.jpg"),
+    Image.asset("img/5.jpg"),
+    Image.asset("img/6.jpg"),
+    Image.asset("img/7.png"),
+    Image.asset("img/8.jpg"),
+    Image.asset("img/9.jpg"),
+    Image.asset("img/10.jpg")
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.black12.withOpacity(0.08),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: EdgeInsets.all(20.5),
-          children: <Widget>[
-            Container(
-              width: 150,
-              height: 50,
-              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.05),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.5),
-                color: Colors.grey.shade50, //Colors.purpleAccent.shade100,
-              ),
-              alignment: Alignment.center,
-              child: Text("Tip Calculator", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  color: Colors.redAccent),
-              ),),
-            // per person share display box
-            Container(
-              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.05),
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.5),
-                color: _purple.withOpacity(0.2), //Colors.purpleAccent.shade100,
-              ),
-
-              child: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget> [
-                      Text("Total Per Person", style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: _purple
-                      ),),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Text("\$ ${calculateTotalTipPerPerson(_billAmount, _personCounter, _tipPercentage)}",style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40.0
-                        ),),
-                      )
-                    ]
-
-                ),
-
-              ),
-
-            ),
-            // per person share display box ends
-
-            // enter bill amount widget, split, divide with share box
-            Container(
-              margin: EdgeInsets.only(top: 20.0),
-              padding: EdgeInsets.all(12.0),
-              decoration: BoxDecoration(
-                  color: Colors.white60,
-                  border: Border.all(
-                      color: Colors.blueGrey.shade700
-                  )
-
-              ),
-              child: Column(
-                children: <Widget>[
-
-                  // bill amount input/display block
-                  TextField(
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    style: TextStyle(color: _purple,
-                        fontWeight: FontWeight.bold),
-                    decoration: InputDecoration(
-                        prefixText: "Bill Amount: ",
-                        prefixIcon: Icon(Icons.attach_money)
-                    ),
-
-                    onChanged: (String value){
-                      try{
-                        _billAmount = double.parse(value);
-                      }catch(exception){
-                        _billAmount = 0.0;
-                      }
-                    },
-
-                  ),
-
-                  // split, decrement, personCounter, increment widget
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      // Split text display
-                      Text("Split", style: TextStyle(
-                          color: Colors.grey.shade700
-                      )
-
-                      ),
-
-                      Row(
-                        // - countDisplay + buttons
-                        children: <Widget>[
-                          // decrement button
-                          InkWell(
-                            onTap: (){
-                              setState(() {
-                                if(_personCounter >1){
-                                  _personCounter--;
-                                }
-                              }
-                              );
-
-                            },
-
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              margin: EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(7.0),
-                                  color: _purple.withOpacity(0.1)
-                              ),
-
-                              child: Center(
-                                child: Text(
-                                  "-", style: TextStyle(
-                                    color: _purple,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 17.0
-                                ),
-
-                                ),
-
-                              ),
-
-                            ),
-
-                          ),
-                          // decrement button ends
-
-                          // person count display
-                          Text("$_personCounter", style: TextStyle(
-                              color: _purple,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17.0
-                          ),
-                          ),
-                          // person count display ends
-
-                          // person count increment button
-                          InkWell(
-                            onTap: (){
-                              setState(() {
-                                _personCounter++;
-                              }
-                              );
-
-                            },
-
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              margin: EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(7.0),
-                                  color: _purple.withOpacity(0.1)
-                              ),
-
-                              child: Center(
-                                child: Text(
-                                  "+", style: TextStyle(
-                                    color: _purple,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 17.0
-                                ),
-
-                                ),
-
-                              ),
-
-                            ),
-
-                          ),
-                          // person count increment button ends
-
-                        ],
-
-                      ),
-
-                    ],
-                    // - countDisplay + buttons end
-
-                  ),
-                  // split, decrement, personCounter, increment widget ends
-
-                  //TIP widget
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      // tip text display
-                      Text("Tip",style: TextStyle(
-                          color: Colors.grey.shade700
-                      ),),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.5, right: 20.5),
-                        child: Text("\$ ${calculateTotalTip(_billAmount, _personCounter, _tipPercentage)}",style: TextStyle(
-                            color: _purple,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17.0
-                        )),
-                      )
-                    ],
-                  ),
-
-                  // SLIDER WIDGET
-                  Column(
-                    children: <Widget>[
-                      Text("$_tipPercentage%", style: TextStyle(
-                          color: _purple,
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold
-                      ),),
-                      Slider(
-                          min: 0, max:100, // enable slider to set the values between min and max (both inclusive)
-                          activeColor: _purple, inactiveColor: Colors.grey,
-                          divisions: 50, // kind of like step counter, 100 pointer slider will be divided into 50 divisions
-
-                          value: _tipPercentage.toDouble(),
-                          onChanged: (double newValue){
-                            setState(() {
-                              _tipPercentage = newValue.round();
-                            });
-                          })
-                    ],
-                  )
-
-                ],
-
-              ),
-
-            ),
-
-          ],
-
+        appBar: AppBar(
+          title: Text("Quiz Nation"),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey,
         ),
-
-      ),
-
-    );
+        backgroundColor: Colors.blueGrey,
+        body: Container(
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Container(
+                      padding: EdgeInsets.only(top: 30.0),
+                      width: 300,
+                      height: 200,
+                      child: imgList[_currentQuestionIndex]),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white30,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    height: 150,
+                    width: 350,
+                    margin: EdgeInsets.all(18.0),
+                    padding: EdgeInsets.all(30.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      questionBank[_currentQuestionIndex].questionText,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () => _prevQuestion(),
+                      child: Icon(Icons.arrow_back, color: Colors.white),
+                    ),
+                    ElevatedButton(
+                        onPressed: () => _checkAnswer(true),
+                        child: Text(
+                          "True",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        )),
+                    ElevatedButton(
+                        onPressed: () => _checkAnswer(false),
+                        child: Text(
+                          "False",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        )),
+                    ElevatedButton(
+                      onPressed: () => _nextQuestion(),
+                      child: Icon(Icons.arrow_forward, color: Colors.white),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
-  calculateTotalTipPerPerson(double billAmount, int splitBy, int tipPercentage){
-    var totalPerPerson = ((calculateTotalTip(billAmount, splitBy, tipPercentage)) + billAmount)/splitBy;
-    return totalPerPerson.toStringAsFixed(2);
-  }
-  calculateTotalTip(double billAmount, int splitBy, int tipPercentage){
-    double totalTip = 0.0;
 
-    if(billAmount < 0 || billAmount.toString().isEmpty || billAmount == null){
-      // do nothing
-    }else{
-      totalTip = (billAmount * tipPercentage / 100);
+  _checkAnswer(bool userChoice) {
+    if (userChoice == questionBank[_currentQuestionIndex].isCorrect) {
+      debugPrint("Correct Answer");
+      final snackBar = SnackBar(
+          content: Text("Correct Answer!", style: TextStyle(fontSize: 20)),
+          padding: EdgeInsets.all(30.0),
+          backgroundColor: Colors.green.shade400,
+          duration: Duration(milliseconds: 500));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      setState(() {
+        _currentQuestionIndex =
+            (_currentQuestionIndex + 1) % questionBank.length;
+      });
+    } else {
+      final snackBar = SnackBar(
+          content: Text(
+            "Incorrect Answer!",
+            style: TextStyle(fontSize: 20),
+          ),
+          padding: EdgeInsets.all(30.0),
+          backgroundColor: Colors.red.shade400,
+          duration: Duration(milliseconds: 500));
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      debugPrint("Incorrect Answer");
     }
-    return totalTip;
+  }
+
+  _prevQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex - 1) % questionBank.length;
+    });
+  }
+
+  _nextQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % questionBank.length;
+    });
   }
 }
